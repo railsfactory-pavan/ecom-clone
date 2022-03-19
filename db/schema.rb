23c79a818e808c870c35b28e8658468e1340f4f2
railsfactory-pavan <pavanrf07@gmail.com>
2022-03-19 17:52:06 +0530

    sti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_083854) do
+ActiveRecord::Schema.define(version: 2022_03_19_121309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -236,6 +236,14 @@ ActiveRecord::Schema.define(version: 2022_03_19_083854) do
 
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "type"
+    t.string "color"
+    t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
